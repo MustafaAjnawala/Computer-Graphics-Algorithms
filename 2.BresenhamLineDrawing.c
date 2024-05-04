e85e986ada01f8fcc5f7bc84ee4d19095db047cf
MustafaAjnawala <117845490+MustafaAjnawala@users.noreply.github.com>
2024-05-04 12:34:01 +0530

    Update 2.BresenhamLineDrawing.c

--- 2.BresenhamLineDrawing.c
+++ 2.BresenhamLineDrawing.c
@@ -3,7 +3,7 @@ void bresenham(int x1, int y1, int x2, int y2)
   int dx, dy, G;
   int sign_x = 1, sign_y = 1;
 
-  // Swap points if x1 > x2
+  // Swap x & y points if x1 > x2
   if (x1 > x2)
   {
     int temp = x1;
@@ -21,7 +21,7 @@ void bresenham(int x1, int y1, int x2, int y2)
 
   // Determine the sign of slope
   if (y2 < y1)
-    sign_y = -1;
+    sign_y = -1; //so will dec y if(y1>y2) and inc y if(y1<y2)
 
   if (dx > dy) // GENTLE SLOPE
   {
@@ -30,14 +30,14 @@ void bresenham(int x1, int y1, int x2, int y2)
       putpixel(x1, y1, LIGHTCYAN);
       if (G >= 0)
       {
-        y1 += sign_y;
+        y1 += sign_y; //will get accordingly inc or dec
         G = G + 2 * (dy - dx);
       }
       else
       {
         G = G + (2 * dy);
       }
-      x1++;
+      x1++; //inc by default as we hv already swapped if(x1>x2)
     }
   }
   else // STEEP SLOPE
@@ -47,14 +47,14 @@ void bresenham(int x1, int y1, int x2, int y2)
       putpixel(x1, y1, LIGHTCYAN);
       if (G >= 0)
       {
-        x1++;
+        x1++; //inc by default as we hv already swapped if(x1>x2)
         G = G + 2 * (dx - dy);
       }
       else
       {
         G = G + (2 * dx);
       }
-      y1 += sign_y;
+      y1 += sign_y; //will get accordingly inc or dec
     }
   }
 }
